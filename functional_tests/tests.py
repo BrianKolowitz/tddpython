0b03e29295eb655449e0a5ce9aadd2bad99ffc9a
Kolowitz <bkolowitz@gmail.com>
2017-09-19 11:49:13 -0400

    fix formatting bugs

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -115,17 +115,18 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
         # Satisfied, they both go back to sleep
 
+
     def test_layout_and_styling(self):
         # Edith goes to the home page
         self.browser.get(self.live_server_url)
-        self.browser.set_window_size(1025, 768)
+        self.browser.set_window_size(1024, 768)
 
         # She notices the input box is nicely centered
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertAlmostEqual(
             inputbox.location['x'] + inputbox.size['width'] / 2,
             512,
-            delta=10
+            delta=5
         )
 
         # She starts a new list and sees the input is nicely
@@ -137,6 +138,5 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.assertAlmostEqual(
             inputbox.location['x'] + inputbox.size['width'] / 2,
             512,
-            delta=10
+            delta=5
         )
-    
