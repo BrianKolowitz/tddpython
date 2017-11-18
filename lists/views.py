a59f3670e718170520d3221595309051c20a5bc5
Kolowitz <bkolowitz@gmail.com>
2017-11-18 12:16:01 -0500

    mostly fixed

--- lists/views.py
+++ lists/views.py
@@ -1,40 +1,29 @@
 from django.shortcuts import redirect, render
-from django.contrib.auth import get_user_model
-
-from lists.forms import ExistingListItemForm, ItemForm, NewListForm
-from lists.models import List
-User = get_user_model()
+from lists.forms import ItemForm
+from lists.models import Item, List
 
 
+# Create your views here.
 def home_page(request):
     return render(request, 'home.html', {'form': ItemForm()})
 
 
-def new_list(request):
-    form = NewListForm(data=request.POST)
-    if form.is_valid():
-        list_ = form.save(owner=request.user)
-        return redirect(list_)
-    return render(request, 'home.html', {'form': form})
-
-
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
-    form = ExistingListItemForm(for_list=list_)
+    form = ItemForm()
     if request.method == 'POST':
-        form = ExistingListItemForm(for_list=list_, data=request.POST)
+        form = ItemForm(data=request.POST)
         if form.is_valid():
-            form.save()
-            return redirect(list_)
+            form.save(for_list=list_)
+            return redirect(list_)            
     return render(request, 'list.html', {'list': list_, "form": form})
 
 
-def my_lists(request, email):
-    owner = User.objects.get(email=email)
-    return render(request, 'my_lists.html', {'owner': owner})
-
-
-def share_list(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    list_.shared_with.add(request.POST['sharee'])
-    return redirect(list_)
+def new_list(request):
+    form = ItemForm(data=request.POST)
+    if form.is_valid():
+        list_ = List.objects.create()
+        form.save(for_list=list_)
+        return redirect(list_)
+    else:
+        return render(request, 'home.html', {"form": form})
