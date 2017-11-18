2010f351d0ded56b3f5d5157936506276d8c4bb2
Kolowitz <bkolowitz@gmail.com>
2017-11-18 12:31:38 -0500

    reformat code

--- lists/forms.py
+++ lists/forms.py
@@ -6,8 +6,8 @@ from lists.models import Item
 EMPTY_ITEM_ERROR = "You can't have an empty list item"
 DUPLICATE_ITEM_ERROR = "You've already got this in your list"
 
-class ItemForm(forms.models.ModelForm):
 
+class ItemForm(forms.models.ModelForm):
     class Meta:
         model = Item
         fields = ('text',)
@@ -21,18 +21,15 @@ class ItemForm(forms.models.ModelForm):
             'text': {'required': EMPTY_ITEM_ERROR}
         }
 
-
     def save(self, for_list):
         self.instance.list = for_list
         return super().save()
 
 
 class ExistingListItemForm(ItemForm):
-
     def __init__(self, for_list, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.instance.list = for_list
-
 
     def validate_unique(self):
         try:
@@ -40,8 +37,6 @@ class ExistingListItemForm(ItemForm):
         except ValidationError as e:
             e.error_dict = {'text': [DUPLICATE_ITEM_ERROR]}
             self._update_errors(e)
-
-
 
     def save(self):
         return forms.models.ModelForm.save(self)
