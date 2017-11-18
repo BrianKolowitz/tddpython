d691df36394ecfce01be2f7e6824ba2af5d115c1
Kolowitz <bkolowitz@gmail.com>
2017-11-18 15:43:15 -0500

    finished chapter 15

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -41,7 +41,7 @@ class NewListTest(TestCase):
 
     def test_for_invalid_input_passes_form_to_template(self):
         response = self.client.post('/lists/new', data={'text': ''})
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
 
     def test_invalid_list_items_arent_saved(self):
         self.client.post('/lists/new', data={'text': ''})
